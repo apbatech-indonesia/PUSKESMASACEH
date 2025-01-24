3b1bf708942efe6bb7711d7aff7464220e897cd3
suwanditaupik <suwanditaupik.jobs@gmail.com>
2025-01-24 16:31:44 +0700

    up skrining

--- src/app/clmaster/skrining/skrining.service.ts
+++ src/app/clmaster/skrining/skrining.service.ts
@@ -13,16 +13,24 @@ export class skriningService {
 
   constructor(public http: HttpClient) { }
 
-  getCluster() {
+  register(data) {
     return new Promise((resolve) => {
-      this.http.post(satusehatUrl + 'cluster/getGroup', null, { headers: this.headers }).subscribe((data) => {
+      this.http.post(satusehatUrl + 'screeningPatient/register', data, { headers: this.headers }).subscribe((data) => {
         resolve(data)
       })
     })
-  }
+  } 
+  
+  getCluster(data) {
+    return new Promise((resolve) => {
+      this.http.post(satusehatUrl + 'cluster/getGroup', data, { headers: this.headers }).subscribe((data) => {
+        resolve(data)
+      })
+    })
+  } 
   
   getSkrinigById(data:any) {
-    let urlMock = 'https://16f3fc19dc1c4661884e009029ca3207.api.mockbin.io/'
+    let urlMock = 'https://9e089e6b77014ff0896a007c23de1b2e.api.mockbin.io/'
     return new Promise((resolve) => {
       this.http.post(satusehatUrl + 'screening/search', data, { headers: this.headers }).subscribe((data) => {
       // this.http.post(urlMock, data, { headers: this.headers }).subscribe((data) => {
