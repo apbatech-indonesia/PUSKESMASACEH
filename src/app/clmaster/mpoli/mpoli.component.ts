104580e8231c6ebd9adedcc7cb0eae4fb64d3997
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2026-09-08 10:37:23 +0700

    update

--- src/app/clmaster/mpoli/mpoli.component.ts
+++ src/app/clmaster/mpoli/mpoli.component.ts
@@ -111,7 +111,7 @@ export class MpoliComponent implements OnInit {
   onPoliChange(kdPoli: string) {
     const poli = this.tpolibpjs.find((x) => x.kdPoli == kdPoli);
 
-    this.polisakit = poli?.poliSakit ?? "";
+    this.polisakit = poli?.statussakit === "true" ? "1" : "2";
   }
 
   klinik() {
